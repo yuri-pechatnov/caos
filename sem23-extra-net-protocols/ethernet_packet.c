08322f25dbacbfa591faf3e32998b84a8c4d7871
Yuri Pechatnov <yuri.pechatnov@gmail.com>
2020-03-25 11:49:12 +0300

    yet another update

--- sem23-extra-net-protocols/ethernet_packet.c
+++ sem23-extra-net-protocols/ethernet_packet.c
@@ -8,7 +8,7 @@
 #include <assert.h>
 #include <errno.h>
 #include <inttypes.h>
-#include <linux/if_ether.h>
+#include <linux/if_ether.h> // вот тут объявлен ethernet_header, там же есть struct ether_arp
 #include <linux/if_packet.h>
 #include <net/ethernet.h>
 #include <net/if.h>
@@ -23,6 +23,15 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+typedef struct {
+    char s[3 * 6];
+} mac_str;
+
+mac_str mac_to_str(u_char* mac) {
+    mac_str ms;
+    sprintf(ms.s, "%02x:%02x:%02x:%02x:%02x:%02x", (int)mac[0], (int)mac[1], (int)mac[2], (int)mac[3], (int)mac[4], (int)mac[5]);
+    return ms;
+}
 
 int main(int argc, char *argv[])
 {
@@ -40,16 +49,17 @@ int main(int argc, char *argv[])
     struct sockaddr_ll device = {
         .sll_ifindex = ifr.ifr_ifindex,
         .sll_halen = ETH_ALEN, // длина адреса (=6 в ethernet) 
-        .sll_addr = {0, 0, 0, 0, 0, 0} // loopback
+        .sll_addr = {0, 0, 0, 0, 0, 0} // loopback (но, кажется, тут может быть любой мусор)
     };
    
     struct {
-        char ethernet_header[14]; // по дефолту наполнен нулями, а это loopback адрес, чего и хочется в этом примере
+        struct ether_header ethernet_header; // по дефолту будет наполнен нулями, а это loopback адрес, чего и хочется в этом примере
         // Вот тут может начинаться хедер протокола более высокого уровня
         uint64_t request_id; // идентификатор, чтобы узнать наш пакет, среди всех проходящих пакетов
         uint64_t value; // имитация полезной нагрузки
-    } request = {.request_id = 17171819, .value = 42424242}, response = {.request_id = 17171819};
-    
+    } request = {.request_id = 17171819, .value = 42424242}, 
+      response = {.request_id = 17171819};
+
     int sendto_res = sendto(sock, &request, sizeof(request), 0,
                             (struct sockaddr*)&device, sizeof(device));
     assert(sendto_res != -1);
@@ -58,11 +68,12 @@ int main(int argc, char *argv[])
         int recv_result = recv(sock, &response, sizeof(response), 0);
         assert(recv_result != -1);
         if (response.value == request.value) {
-            printf("Hey, I got it! response.value = %" PRIu64 "\n", response.value);
+            printf("Hey, I got it! response.value = %" PRIu64 ", eth_type = %#06x, src_mac = %s, dst_mac = %s\n", 
+                   response.value, response.ethernet_header.ether_type,
+                   mac_to_str(response.ethernet_header.ether_shost).s, mac_to_str(response.ethernet_header.ether_dhost).s);
             break;
         }
     }
-   
     close(sock);
     return 0;
 }
---
+++ sem23-extra-net-protocols/ping.c
@@ -0,0 +1,131 @@
+// %%cpp ping.c
+// %run gcc -Wall -Werror -fsanitize=thread ping.c -lpthread -o ping.exe
+// %# Чтобы использовать SOCK_RAW нужны capabilities для исполняемого файла
+// %run echo $PASSWORD | sudo -S setcap cap_net_raw,cap_net_admin+eip ./ping.exe 2>/dev/null
+// %run timeout 5 ./ping.exe ya.ru
+
+#include <arpa/inet.h>
+#include <assert.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/if_ether.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#define MAXPACKET   4096    
+#define DATALEN 64     
+
+const char* const pretty_icmp_type[] = {
+    "Echo Reply", "ICMP 1", "ICMP 2", "Dest Unreachable", "Source Quench", "Redirect", "ICMP 6", 
+    "ICMP 7", "Echo Request", "ICMP 9", "ICMP 10", "Time Exceeded", "Parameter Problem",
+    "Timestamp", "Timestamp Reply", "Info Request", "Info Reply"
+};
+
+uint64_t time_ns() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * 1000000000L + ts.tv_nsec;
+}
+
+int check_sum(u_short * addr, int len) {
+    u_short *w = addr;
+    int sum = 0;
+    while (len > 1) {
+        sum += *w++;
+        len -= 2;
+    }
+    sum += (len == 1) ? *(u_char *)w : 0;    
+    sum = (sum >> 16) + (sum & 0xffff); 
+    return ~((sum >> 16) + sum);
+}
+
+typedef struct {
+    int socket_fd;
+    struct sockaddr* whereto;
+    int ident;
+} send_ping_forever_args_t;
+
+void send_ping(send_ping_forever_args_t* arg, int icmp_seq_no) {
+    static u_char outpack[MAXPACKET];
+    memset(outpack, 0, sizeof(outpack));
+    struct icmp *icp = (struct icmp *) outpack;
+    icp->icmp_type = ICMP_ECHO;
+    icp->icmp_code = 0;
+    icp->icmp_cksum = 0;
+    icp->icmp_seq = icmp_seq_no;
+    icp->icmp_id = arg->ident;     
+    *(uint64_t*)icp->icmp_data = time_ns(); 
+    
+    icp->icmp_cksum = check_sum((u_short*)(void*)icp, DATALEN);
+    int res = sendto(arg->socket_fd, outpack, DATALEN, 0, arg->whereto, sizeof(struct sockaddr_in));
+    assert(res == DATALEN);
+}
+
+void send_ping_forever(send_ping_forever_args_t* arg) {
+    for (int icmp_seq_no = 0; 1; ++icmp_seq_no) {
+        send_ping(arg, icmp_seq_no);
+        sleep(1);
+    }
+}
+
+void parse_and_print(u_char* buf, int length, struct sockaddr_in* from, int ident) {
+    struct ip *ip = (struct ip *) buf; // откусываем ip-заголовок
+    int hlen = ip->ip_hl << 2;
+    length -= hlen;
+    assert(length >= ICMP_MINLEN);
+    struct icmp *icp = (struct icmp *)(buf + hlen);
+    
+    if (icp->icmp_id != ident)
+        return; // эти запросы отправили точно не мы
+
+    // обратите внимание, что собственные отправленные запросы мы тоже получаем на вход
+    printf("%d bytes from %s: icmp_type=%d (%s) icmp_seq=%d icmp_code=%d time=%d ns\n",
+        length, inet_ntoa(from->sin_addr), icp->icmp_type, pretty_icmp_type[icp->icmp_type], 
+           icp->icmp_seq, icp->icmp_code, (int)(time_ns() - (*(uint64_t *)icp->icmp_data)));
+}
+
+
+int main(int argc, char **argv) {
+    assert(argc == 2);
+    
+    int ident = getpid() & 0xFFFF;
+    
+    struct hostent *hp = gethostbyname(argv[1]);
+    assert(hp && hp->h_addrtype == AF_INET);
+    
+    struct sockaddr_in whereto = {.sin_family = hp->h_addrtype};
+    memcpy(&whereto.sin_addr, hp->h_addr, hp->h_length);
+
+    // struct protoent *proto = getprotobyname("icmp"); // use proto->p_proto // можно так вместо IPPROTO_ICMP
+    // с PF_INET получается интересная комбинация, при отправке мы IP-хедер не указываем, а при получении получаем IP-хедер
+    int socket_fd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
+    assert(socket_fd >= 0);
+    
+    printf("PING %s (%s): %d data bytes\n", hp->h_name, inet_ntoa(whereto.sin_addr), DATALEN);  
+    pthread_t thread;
+    send_ping_forever_args_t args = {.socket_fd = socket_fd, .whereto = (struct sockaddr *)&whereto, .ident = ident};
+    assert(pthread_create(&thread, NULL, (void* (*)(void*))send_ping_forever, (void*)&args) == 0);
+      
+    // Вечно получаем ответы
+    while (1) { 
+        u_char packet[MAXPACKET];
+        struct sockaddr_in from;
+        socklen_t fromlen = sizeof(from);
+        int recv_size = recvfrom(socket_fd, packet, sizeof(packet), 0, (struct sockaddr*)&from, &fromlen);
+        if (recv_size < 0) {
+            assert(errno == EINTR);
+            continue;
+        }
+        parse_and_print(packet, recv_size, &from, ident);
+    }
+}
+
